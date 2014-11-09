4cffceae5b8c3f60dbbafef4cc3c70aa94e8dfa7
Nikolay Valkov <nikolay.valkov@abv.bg>
2014-11-09 18:35:26 +0200

    I have created my aplication

---
+++ 1301681029_NikolaiValkov_project/1301681029_NikolaiValkov_project/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace _1301681029_NikolaiValkov_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLbl});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 228);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(383, 22);
+            this.statusStrip1.TabIndex = 45;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(118, 17);
+            this.statusLbl.Text = "toolStripStatusLabel1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(215, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 18);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "View information";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(195, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(179, 134);
+            this.listBox1.TabIndex = 43;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(195, 199);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(179, 23);
+            this.btnUpdate.TabIndex = 42;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(114, 199);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 41;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(33, 199);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 40;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(89, 166);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 39;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(11, 169);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(56, 13);
+            this.lblPass.TabIndex = 38;
+            this.lblPass.Text = "Password:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(89, 127);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 37;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(29, 130);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(38, 13);
+            this.lblEmail.TabIndex = 36;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(89, 88);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 35;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(6, 91);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(61, 13);
+            this.lblUserName.TabIndex = 34;
+            this.lblUserName.Text = "User name:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(89, 51);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 33;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(48, 54);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(19, 13);
+            this.lblId.TabIndex = 32;
+            this.lblId.Text = "Id:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(40, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 18);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Input information";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 250);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 1301681029_NikolaiValkov_project/1301681029_NikolaiValkov_project/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace _1301681029_NikolaiValkov_project
+{
+    public partial class Form1 : Form
+    {
+
+        private void crudFunc(string zaqvka)
+        {
+            SqlConnection sqlc = new SqlConnection(@"Data Source=НИКСАН-PC\SQLEXPRESS;Initial Catalog=users :);Integrated Security=True");
+            try
+            {
+                sqlc.Open();
+                statusLbl.Text = "Connection state succesfully";
+                SqlCommand cmd = sqlc.CreateCommand();
+                cmd.CommandText = zaqvka;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    statusLbl.Text = "Record inserted successfully!";
+                    sqlc.Close();
+                }
+                catch (Exception le)
+                {
+                    MessageBox.Show(le.Message);
+                    statusLbl.Text = "Record fail!";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        public void reader()
+        {
+            listBox1.Items.Clear();
+            //Data Source=НИКСАН-PC\SQLEXPRESS;Initial Catalog=c-sharp-project;Integrated Security=True"
+            //Data Source=НИКСАН-PC\SQLEXPRESS;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False
+            SqlConnection sqlc = new SqlConnection(@"Data Source=НИКСАН-PC\SQLEXPRESS;Initial Catalog=users :);Integrated Security=True");
+            try
+            {
+                sqlc.Open();
+                statusLbl.Text = "Connection state succesfully";
+                SqlCommand cmd = sqlc.CreateCommand();
+                cmd.CommandText = @"SELECT ID, NAME, ADDRESS, PASSWORD from Table_1";
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    listBox1.Items.Add(id.ToString() + " " + reader.GetString(1) + " " + reader.GetString(2) + " " + reader.GetString(3));
+                }
+                reader.Close();
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    statusLbl.Text = "Readed successfully!";
+                    sqlc.Close();
+                }
+                catch (Exception le)
+                {
+                    MessageBox.Show(le.Message);
+                    statusLbl.Text = "Can't read!";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reader();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //crudFunc("Insert into Table_1( [ID],[NAME],[ADDRESS],[PASSWORD] ) values('" + Convert.ToInt32(txtId.Text) + "','" + txtUserName.Text + "','" + txtEmail.Text + "','" + txtPass.Text + "')");
+            if (txtId.Text == null && txtUserName.Text == null && txtPass.Text == null && txtEmail.Text == null)
+            {
+                statusLbl.Text = "Не сте попълнили някое поле!";
+            }
+            else
+            {
+                int isInteger;
+                if (int.TryParse(txtId.Text, out isInteger))
+                {
+                    if (txtUserName.Text.Length < 50 && txtPass.Text.Length < 50 && txtEmail.Text.Length < 50)
+                    {
+                        crudFunc("Insert into Table_1( [ID],[NAME],[ADDRESS],[PASSWORD] ) values('" + Convert.ToInt32(txtId.Text) + "','" + txtUserName.Text + "','" + txtEmail.Text + "','" + txtPass.Text + "')");
+                        statusLbl.Text = "Записът е успешен!";
+                        reader();
+                    }
+                    else
+                    {
+                        statusLbl.Text = "Error";
+                    }
+                }
+                else
+                {
+                    statusLbl.Text = "ID-то полето не е число!";
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selecteditem = listBox1.SelectedItem.ToString();
+                string selectedItemId = selecteditem.Substring(0, selecteditem.IndexOf(" "));
+                if (selectedItemId != "")
+                {
+                    crudFunc(@"Delete from Table_1 WHERE (ID='" + selectedItemId + "')");
+                    statusLbl.Text = "Записът е изтрит успешно!";
+                    reader();
+                }
+                else
+                {
+                    statusLbl.Text = "Не сте избрали запис!";
+                }
+            }
+            else
+            {
+                statusLbl.Text = "Не сте избрали запис!";
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == null && txtPass.Text == null && txtEmail.Text == null)
+            {
+                statusLbl.Text = "Не сте попълнили някое поле!";
+            }
+            else
+            {
+                string selecteditem = listBox1.SelectedItem.ToString();
+                string selectedItemId = selecteditem.Substring(0, selecteditem.IndexOf(" "));
+                if (listBox1.SelectedItem != null)
+                {
+                    if (txtUserName.Text.Length < 50 && txtPass.Text.Length < 50 && txtEmail.Text.Length < 50)
+                    {
+                        crudFunc(@"Update Table_1 
+                        SET NAME ='" + txtUserName.Text + "',ADDRESS='" + txtEmail.Text + "', PASSWORD='" + txtPass.Text + "'WHERE (ID='" + selectedItemId + "')");
+                        statusLbl.Text = "Ъпдейта е успешен!";
+                        reader();
+                    }
+                    else
+                    {
+                        statusLbl.Text = "Error";
+                    }
+                }
+                else
+                {
+                    statusLbl.Text = "Не сте избрали запис за update!";
+                }
+            }
+        }
+    }
+}
