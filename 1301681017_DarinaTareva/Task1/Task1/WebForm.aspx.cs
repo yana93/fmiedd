3c2a2f6c917fd29d4bb668c0dff1ae31d636c7db
dtareva <d_tareva@abv.bg>
2014-11-09 18:53:00 +0200

    yesss ^_^

---
+++ 1301681017_DarinaTareva/Task1/Task1/WebForm.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Task1
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(@"Server=USER-PC;Database=Project_Users;Trusted_Connection=True;");
+
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Insert into Project_Users values ('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "')";
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            Response.Redirect("WebForm.aspx");
+           
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+         
+
+
+         } 
+        }
+    }
