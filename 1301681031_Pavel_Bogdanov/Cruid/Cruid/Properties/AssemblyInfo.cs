01b6ba35be31201b30ed329b0df8176ef5427be6
pavel945 <pavelski1994@gmail.com>
2014-11-09 17:42:31 +0200

    I have created my app

--- 1301681031_Pavel_Bogdanov/Cruid/Cruid/Properties/AssemblyInfo.cs
+++ 1301681031_Pavel_Bogdanov/Cruid/Cruid/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CRUD")]
+[assembly: AssemblyTitle("Cruid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CRUD")]
+[assembly: AssemblyProduct("Cruid")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3bdc5efd-b6cd-4ca3-b50d-1e020447f32f")]
+[assembly: Guid("cc3a6843-ee28-4201-a205-25d16a56470c")]
 
 // Version information for an assembly consists of the following four values:
 //
