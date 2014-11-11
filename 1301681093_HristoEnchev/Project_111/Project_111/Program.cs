42fff612790520ea492d5ee79c128ccd7b5481cb
hrito1 <hr_enchev@abv.bg>
2014-11-11 11:44:58 +0200

    Third Commit

---
+++ 1301681093_HristoEnchev/Project_111/Project_111/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App start = new App();
+            start.FistPage();
+            start.Start();
+            Console.ReadKey();
+        }
+    }
+}
