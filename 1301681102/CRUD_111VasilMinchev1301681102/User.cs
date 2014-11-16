8d2418b5536f9fa5927ba33a1c1e814e6746e172
joraldinio <vasil.minchevv@abv.bg>
2014-11-16 07:37:07 +0200

    CRUDVasilMinchev
    sled 200 chasa ..

---
+++ 1301681102/CRUD_111VasilMinchev1301681102/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_111VasilMinchev1301681102
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int ID { get; set; }
+    }
+}
